815aeb776ef28effc0ede5e335e0f5462026d034
99002552 <71484509+99002552@users.noreply.github.com>
2020-09-19 08:53:09 +0530

    Update test.c

--- Cafteria_Management/3_Implementation/test/test.c
+++ Cafteria_Management/3_Implementation/test/test.c
@@ -7,7 +7,7 @@ void setUp() {}
 //Required by the unity framework
 void tearDown() {}
 
-void testing_prime_number(void)
+void testing_canteen(void)
 {
     TEST_ASSERT_EQUAL(6, start(6));
     TEST_ASSERT_EQUAL(5, start(4));
@@ -18,7 +18,6 @@ int main(void)
 {
     //Initiating the unity framework
     UNITY_BEGIN();
-    RUN_TEST(testing_prime_number);
-    RUN_TEST(testing_forlarge_prime_number);
+    RUN_TEST(testing_canteen);
     return UNITY_END();
 }
